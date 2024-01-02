af43fa9dfe5d125ead381b4094e500d069cda989
lhhernandez <hhernandez.isc@gmail.com>
2024-01-01 20:40:41 -0600

    New Place to Play

---
+++ CodeCoverage/CoveredFile.swift
@@ -0,0 +1,18 @@
+//
+//  CoveredFile.swift
+//  CodeCoverage
+//
+//  Created by administrador on 01/01/24.
+//
+
+import Foundation
+
+class CoveredClass {
+    static func max(_ x: Int, _ y: Int) -> Int{
+        if x < y {
+            return y
+        }else {
+            return x
+        }
+    }
+}
---
+++ CodeCoverageTests/CoveredClassTests.swift
@@ -0,0 +1,16 @@
+//
+//  CoveredClassTests.swift
+//  CodeCoverageTests
+//
+//  Created by administrador on 01/01/24.
+//
+
+import XCTest
+
+final class CoveredClassTests: XCTestCase {
+
+    func test_zero(){
+        XCTFail("Tests not yet implemented in CoveredClassTests")
+    }
+    
+}
