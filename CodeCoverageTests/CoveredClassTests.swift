270a30df21e161c6d8dd82ff538f327aa2d58a07
lhhernandez <hhernandez.isc@gmail.com>
2024-01-08 22:02:42 -0600

    End of chapter 3

--- CodeCoverageTests/CoveredClassTests.swift
+++ CodeCoverageTests/CoveredClassTests.swift
@@ -6,11 +6,40 @@
 //
 
 import XCTest
+@testable import CodeCoverage
 
 final class CoveredClassTests: XCTestCase {
 
-    func test_zero(){
-        XCTFail("Tests not yet implemented in CoveredClassTests")
+    func test_max1And2_shouldReturnSomething(){
+        let result = CoveredClass.max(1, 2)
+    }
+    
+    func test_max_with1And2_shouldReturn2(){
+        let result = CoveredClass.max(1, 2)
+        XCTAssertEqual(result, 2)
+    }
+    
+    func test_max_with3And2_shouldReturn3(){
+        let result = CoveredClass.max(3, 2)
+        XCTAssertEqual(result, 3)
+    }
+    
+    func test_commaSeparated_from2To4_shouldReturn234(){
+        let result = CoveredClass.commaSeparated(from: 2, to: 4)
+        XCTAssertEqual(result, "2,3,4")
+    }
+    
+    func test_commaSeparated_from2to2_shouldReturnSomething(){
+        let result = CoveredClass.commaSeparated(from: 2, to: 2)
+        XCTAssertEqual(result, "2")
+    }
+    
+    func test_area_withWidth7_shouldBe49(){
+        let sut = CoveredClass()
+        
+        sut.width = 7
+        
+        XCTAssertEqual(sut.area, 49)
     }
     
 }
