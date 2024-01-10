7bbb31498586e245353147f20743092a21fc0930
lhhernandez <hhernandez.isc@gmail.com>
2024-01-09 21:51:43 -0600

    Initial Commit

---
+++ AppLaunch/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppLaunch
+//
+//  Created by administrador on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
